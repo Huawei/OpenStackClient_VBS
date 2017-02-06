9864f724fbd02e78fe243952e3e72c1c3befc2be
QianBiao NG <xmufive@gmail.com>
2017-02-06 21:45:48 +0800

    finish all unittests

--- vbclient/tests/osc/v1/test_job.py
+++ vbclient/tests/osc/v1/test_job.py
@@ -12,6 +12,8 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 #
+import copy
+
 import mock
 import six
 from osc_lib import utils
@@ -47,7 +49,7 @@ class TestShowJob(base.VolumeBackupBaseTestCase):
         super(TestShowJob, self).setUp()
         self.cmd = job.ShowJob(self.app, None)
 
-    def test_create_volume_backup(self, mocked_get):
+    def test_show_job_success(self, mocked_get):
         args = [
             "job-id-1",
         ]
@@ -72,28 +74,63 @@ class TestShowJob(base.VolumeBackupBaseTestCase):
             "Status",
         ])
 
-        # ????
-        entities = "backup_id='c6be4287-6707-4f5b-84ef-07013851b60d', " \
-            "bks_create_volume_name='autobk_volume', " \
-            "snapshot_id='34f14aeb-cede-4e1b-8d9f-14a2c43bae9f', " \
-            "volume_id='a5109cba-1b1f-4d40-b3a9-753bc808b66a' "
+        entities = six.text_type("backup_id='ba5401a2-7cd2-4c01-8c0d-c936ab412d6d', "
+                                 "bks_create_volume_name='autobk_volume', "
+                                 "snapshot_id='a77a96bf-dd18-40bf-a446-fdcefc1719ec', "
+                                 "volume_id='7e5fdc5a-5e36-4b22-8bcc-7f17037290cc'")
 
-
-        print tuple([
+        self.assertEqual(data, (
             "4010b39b5281d3590152874bfa3b1604",
             "bksCreateBackup",
             "2016-01-28T16:14:09.466Z",
             "2016-01-28T16:25:27.690Z",
             entities,
             "SUCCESS",
+        ))
+
+
+    def test_show_failed_job(self, mocked_get):
+        args = [
+            "job-id-1",
+        ]
+        verify_args = [
+            ("job_id", "job-id-1"),
+        ]
+        parsed_args = self.check_parser(
+            self.cmd, args, verify_args
+        )
+
+        _job = copy.deepcopy(self.job)
+        _job["error_code"] = "10086"
+        _job["fail_reason"] = "unittest"
+        mocked_get.return_value = resource.Job(
+            mock.Mock(), _job, "request-id"
+        )
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.assertEqual(columns, [
+            "Id",
+            "Type",
+            "Begin Time",
+            "End Time",
+            "Entities",
+            "Error Code",
+            "Fail Reason",
+            "Status",
         ])
 
-        print entities
-        self.assertEqual(tuple(data), tuple([
+        entities = six.text_type("backup_id='ba5401a2-7cd2-4c01-8c0d-c936ab412d6d', "
+                                 "bks_create_volume_name='autobk_volume', "
+                                 "snapshot_id='a77a96bf-dd18-40bf-a446-fdcefc1719ec', "
+                                 "volume_id='7e5fdc5a-5e36-4b22-8bcc-7f17037290cc'")
+
+        self.assertEqual(data, (
             "4010b39b5281d3590152874bfa3b1604",
             "bksCreateBackup",
             "2016-01-28T16:14:09.466Z",
             "2016-01-28T16:25:27.690Z",
             entities,
+            "10086",
+            "unittest",
             "SUCCESS",
-        ]))
+        ))
